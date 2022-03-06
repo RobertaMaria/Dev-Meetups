07a9136072e0a4da287259ce264d3b57f1bba9f2
RobertaMaria <robertaji26@gmail.com>
2022-03-06 18:29:34 -0300

    refatora o código, delegando responsabilidades

--- app/src/main/java/br/com/alura/meetups/firebase/MeetupsFirebaseMessagingService.kt
+++ app/src/main/java/br/com/alura/meetups/firebase/MeetupsFirebaseMessagingService.kt
@@ -1,14 +1,13 @@
 package br.com.alura.meetups.firebase
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.graphics.drawable.toBitmap
 import br.com.alura.meetups.R
 import br.com.alura.meetups.model.Dispositivo
 import br.com.alura.meetups.notifications.IDENTIFICADOR_DO_CANAL
+import br.com.alura.meetups.notifications.Notificacao
 import br.com.alura.meetups.preferences.FirebaseTokenPreferences
 import br.com.alura.meetups.repository.DispositivoRepotisitory
 import coil.imageLoader
@@ -20,12 +19,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
-
-
 class MeetupsFirebaseMessagingService : FirebaseMessagingService() {
 
     private val dispositivoRepotisitory: DispositivoRepotisitory by inject()
-    private val preferences : FirebaseTokenPreferences by inject()
+    private val preferences: FirebaseTokenPreferences by inject()
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
@@ -38,49 +35,11 @@ class MeetupsFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
 
-        Log.i("TAG", "onMessageReceived: Notificacao recebida: ${remoteMessage.notification?.title}")
+        Log.i("TAG",
+            "onMessageReceived: Notificacao recebida: ${remoteMessage.notification?.title}")
         Log.i("TAG", "onMessageReceived: Dado recebida ${remoteMessage.data}")
-        val data = remoteMessage.data
 
-        val gerenciadorDeNotificacao = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
-        criaCanal(gerenciadorDeNotificacao)
-
-        mostraNotificacao(data, gerenciadorDeNotificacao)
-
-
-    }
-
-    private fun mostraNotificacao(
-        data: Map<String, String>,
-        gerenciadorDeNotificacao: NotificationManager,
-    ) {
-        CoroutineScope(Dispatchers.IO).launch {
-            val request = ImageRequest.Builder(this@MeetupsFirebaseMessagingService)
-                .data(data["imagem"])
-                .build()
-
-            val imagem = imageLoader.execute(request).drawable?.toBitmap()
-
-            val notificacao = NotificationCompat.Builder(this@MeetupsFirebaseMessagingService,
-                IDENTIFICADOR_DO_CANAL)
-                .setContentTitle(data["titulo"])
-                .setContentText(data["descricao"])
-                .setSmallIcon(R.drawable.ic_acao_novo_evento)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                //.setStyle(NotificationCompat.BigTextStyle()//para texto expansivos, grandes
-                //.bigText(data["descricao"]))
-                .setStyle(NotificationCompat.BigPictureStyle()//imagem grande
-                    .bigPicture(imagem)
-                    .bigLargeIcon(null))
-                .setLargeIcon(imagem)
-                .build()
-
-            gerenciadorDeNotificacao.notify(1, notificacao)
-        }
-    }
-
-    private fun criaCanal(gerenciadorDeNotificacao: NotificationManager) {
+        Notificacao(this).mostra(remoteMessage.data)
 
     }
 }
